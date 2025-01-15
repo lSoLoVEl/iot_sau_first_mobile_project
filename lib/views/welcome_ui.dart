ed60f09d28511c0b8b95ccb050a29d0577493034
lSoLoVEl <S6519410044@sau.ac.th>
2025-01-15 12:20:41 +0700

    v1.2:ทำหน้าจอแรกเข้าใช้ของแอป พร้อมมีร

---
+++ lib/views/welcome_ui.dart
@@ -0,0 +1,98 @@
+// ignore_for_file: sort_child_properties_last
+
+import 'package:flutter/material.dart';
+
+class WelcomeUI extends StatefulWidget {
+  const WelcomeUI({super.key});
+
+  @override
+  State<WelcomeUI> createState() => _WelcomeUIState();
+}
+
+class _WelcomeUIState extends State<WelcomeUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.1,
+            ),
+            Image.asset(
+              'assets/images/logo.png',
+              //width: 250.0, คนใช้หน้าจอใหญ่ก๋เล็กเท่าเดิม
+              width: MediaQuery.of(context).size.width * 0.7,
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.1,
+            ),
+            Text(
+              'IOT SAU App',
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.height * 0.04,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Welcome to IOT SAU APP',
+            ),
+            Text(
+              'Created by Sorawit SAU',
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.1,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                OutlinedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'LOGIN',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                          7.0), //แนะนำให้ใส่เป้นตัวเลขกำหนดไปเลย
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.height * 0.04,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'SIGNUP',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: Size(
+                      150,50
+                    ),
+                    backgroundColor: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                          7.0), //แนะนำให้ใส่เป้นตัวเลขกำหนดไปเลย
+                ),
+                    ),
+                  ),
+                
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
